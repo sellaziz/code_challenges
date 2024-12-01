9c6a9a7682b278b961fdc4b0a997a876730e1d8c
Aziz S <sellaziz.code@outlook.fr>
2024-12-01 23:27:41 +0100

    adventofcode: 2024: day1: solve part2

--- adventofcode/2024/day1/src/main.rs
+++ adventofcode/2024/day1/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
 use std::env;
 use std::fs;
 use std::io;
@@ -6,13 +8,18 @@ use std::process::exit;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let valid_args: HashSet<&str> = ["part1", "part2"].iter().cloned().collect();
 
-    if args.len() < 2 {
+    if args.len() < 3 {
         exit(1);
     }
 
-    let file_path = &args[1];
+    let part = &args[1];
+    let file_path = &args[2];
 
+    if !valid_args.contains(part.as_str()) {
+        exit(1);
+    }
     println!("Parsing {file_path}...");
 
     let contents = fs::File::open(file_path).expect("Should have been able to read the file");
@@ -37,14 +44,40 @@ fn main() {
     list1.sort();
     list2.sort();
 
-    let distance_list: Vec<i32> = list1
-        .iter()
-        .zip(list2.iter())
-        .map(|(a, b)| (a - b).abs())
-        .collect();
+    if part == "part1" {
+        let distance_list: Vec<i32> = list1
+            .iter()
+            .zip(list2.iter())
+            .map(|(a, b)| (a - b).abs())
+            .collect();
 
-    // println!("Distances: {:?}", distance_list);
+        // println!("Distances: {:?}", distance_list);
 
-    let total: i32 = distance_list.iter().sum();
-    println!("Sum: {}", total);
+        let total: i32 = distance_list.iter().sum();
+        println!("Sum: {}", total);
+    } else if part == "part2" {
+        let mut counts2 = HashMap::new();
+
+        for &num in &list2 {
+            *counts2.entry(num).or_insert(0) += 1;
+        }
+
+        // println!("Count 2: {:?}", counts2);
+
+        let similarities: Vec<i32> = list1
+            .iter()
+            .filter_map(|value| {
+                if counts2.contains_key(value) {
+                    Some(value * counts2[value])
+                } else {
+                    None
+                }
+            })
+            .collect();
+
+        // println!("Similarities: {:?}", similarities);
+
+        let total: i32 = similarities.iter().sum();
+        println!("Sum: {}", total);
+    }
 }
